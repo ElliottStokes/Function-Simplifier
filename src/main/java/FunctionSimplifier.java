5b440f6eed432704e8291568f8f8cb5ffcaa9140
Elliott Stokes <15624492@students.lincoln.ac.uk>
2022-03-05 16:07:48 +0000

    Fixed main function declaration

--- src/main/java/FunctionSimplifier.java
+++ src/main/java/FunctionSimplifier.java
@@ -1,5 +1,5 @@
 public class FunctionSimplifier {
-    public static void main() {
+    public static void main(String[] args) {
         System.out.println("Function Simplifier");
         Addition test = new Addition(2, 3);
         test.print();
