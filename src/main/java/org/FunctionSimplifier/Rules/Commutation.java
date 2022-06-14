ba1bebd4cdc14a06a6187d2e9b0fe58f8bad7d8d
Elliott Stokes <erstokes1@sheffield.ac.uk>
2022-06-14 11:07:06 +0100

    Create Rules directory with Commutation and Distributive rules

---
+++ src/main/java/org/FunctionSimplifier/Rules/Commutation.java
@@ -0,0 +1,12 @@
+/* The Commutation Law
+ *
+ * A + B = B + A
+ *
+ * The order of the values around the operator do not change the result
+ */
+
+package org.FunctionSimplifier.Rules;
+
+public interface Commutation {
+    void applyCommutation();
+}
---
+++ src/main/java/org/FunctionSimplifier/Rules/Distributive.java
@@ -0,0 +1,12 @@
+/* The Distributive law
+ *
+ * A(B + C) = AB + AC
+ *
+ */
+
+package org.FunctionSimplifier.Rules;
+
+public interface Distributive {
+    void applyDistributive();
+}
+
