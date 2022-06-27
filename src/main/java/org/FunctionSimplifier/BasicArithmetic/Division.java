2b2c53767cb0a8721e8de78e1eb62abeea4945c0
Elliott Stokes <erstokes1@sheffield.ac.uk>
2022-06-27 14:23:24 +0100

    Created Divison class with toString and evaluate

---
+++ src/main/java/org/FunctionSimplifier/BasicArithmetic/Division.java
@@ -0,0 +1,47 @@
+package org.FunctionSimplifier.BasicArithmetic;
+
+import org.FunctionSimplifier.Function;
+import org.FunctionSimplifier.Variable;
+
+public class Division extends Function {
+    private Variable left;
+    private Variable right;
+
+    public Division(int _left, int _right) {
+        this.left = new Variable(_left);
+        this.right = new Variable(_right);
+    }
+
+    public Division(double _left, double _right) {
+        this.left = new Variable(_left);
+        this.right = new Variable(_right);
+    }
+
+    public Division(Variable _left, Variable _right) {
+        this.left = _left;
+        this.right = _right;
+    }
+
+    public String toString() {
+        return this.left + " / " + this.right;
+    }
+
+    public Variable evaluate() {
+        if (this.left.hasLabel() && this.left.getLabel().equals(this.right.getLabel())) {
+            double exponentValue = this.left.getExponent() - this.right.getExponent();
+            double constantValue = this.left.getConstant() / this.right.getConstant();
+
+            if (exponentValue == 0) {
+                return new Variable(constantValue);
+            } else {
+                return new Variable(this.left.getLabel(), constantValue, exponentValue);
+            }
+        } else {
+            return new Variable(this.left.evaluate() / this.right.evaluate());
+        }
+    }
+
+    public Variable evaluate(double leftValue, double rightValue) {
+        return new Variable(this.left.evaluate(leftValue) / this.right.evaluate(rightValue));
+    }
+}
