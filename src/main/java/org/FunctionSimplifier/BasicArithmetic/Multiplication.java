9d7cdedd0c1fe217a0273667a0cf64610195cbae
Elliott Stokes <erstokes1@sheffield.ac.uk>
2022-06-29 18:50:43 +0100

    Added hasLabel method for Variable class

--- src/main/java/org/FunctionSimplifier/BasicArithmetic/Multiplication.java
+++ src/main/java/org/FunctionSimplifier/BasicArithmetic/Multiplication.java
@@ -62,7 +62,7 @@ public class Multiplication extends Function implements Commutation, Distributiv
 
     public Variable evaluate() {
         // When the variables are only constant variables
-        if (this.left.getLabel().equals("") && this.right.getLabel().equals(""))
+        if (!this.left.hasLabel() && !this.right.hasLabel())
             return new Variable(this.left.evaluate() * this.right.evaluate());
         // When the variables include the same labels
         else if (this.left.getLabel().equals(this.right.getLabel()))
@@ -70,7 +70,7 @@ public class Multiplication extends Function implements Commutation, Distributiv
                     this.left.getConstant() * this.right.getConstant(),
                     this.left.getExponent() + this.right.getExponent());
         // When the left side of the operation includes a label but the other is constant
-        else if (this.left.getLabel().equals("")) {
+        else if (!this.left.hasLabel()) {
             this.right.setConstant(this.right.getConstant() * this.left.getConstant());
             return this.right;
         }
