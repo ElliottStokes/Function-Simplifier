e52824ec11c7d0103487ca2b26952efdd69e34e3
Elliott Stokes <erstokes1@sheffield.ac.uk>
2022-07-20 12:14:21 +0100

    Removed functionString attribute, unnecessary with the toString() method.

--- src/main/java/org/FunctionSimplifier/Function.java
+++ src/main/java/org/FunctionSimplifier/Function.java
@@ -1,50 +1,40 @@
 package org.FunctionSimplifier;
 
+import org.FunctionSimplifier.BasicArithmetic.Addition;
+import org.FunctionSimplifier.BasicArithmetic.Arithmetic;
 import org.FunctionSimplifier.BasicArithmetic.Multiplication;
 
 public class Function {
-    private String functionString;
     private Object body;
 
     public Function() {
         this.body = new Variable(0);
-        this.functionString = this.body.toString();
     }
 
     public Function(int value) {
         this.body = new Variable(value);
-        this.functionString = this.body.toString();
     }
 
     public Function(double value) {
         this.body = new Variable(value);
-        this.functionString = this.body.toString();
     }
 
     public Function(Variable variable) {
         this.body = variable;
-        this.functionString = this.body.toString();
-    }
-
-    public Function(Variable variable, String funcString) {
-        this.body = variable;
-        this.functionString = funcString;
     }
 
     public Function(Multiplication multiplication) {
         this.body = multiplication;
-        this.functionString = this.body.toString();
     }
 
-    public Function(Multiplication multiplication, String funcString) {
-        this.body = multiplication;
-        this.functionString = funcString;
+    public Function(Addition addition) {
+        this.body = addition;
     }
 
-    public void print() {
-        // Print the formatted function
-        System.out.println(this.functionString);
+    public Function(Function function) {
+        this.body = function;
     }
+
 
     public boolean isVariable() {
         return this.body instanceof Variable;
@@ -55,6 +45,41 @@ public class Function {
     }
 
     public String toString() {
-        return this.functionString;
+        return this.body.toString();
+    }
+
+    public Function evaluate() {
+        if (this.body instanceof Variable)
+            return new Function((Variable) this.body);
+        else if (this.body instanceof Multiplication)
+            return ((Multiplication) this.body).evaluate();
+        else if (this.body instanceof Addition)
+            return ((Addition) this.body).evaluate();
+        else
+            return new Function(0);
+    }
+
+    public Function getRightVariable() {
+        Function subFunction = ((Arithmetic) this.body).getRight();
+        while (!subFunction.isVariable()) {
+            subFunction = ((Arithmetic) this.body).getRight();
+        }
+        return subFunction;
+    }
+
+    public Function getRightFunction() {
+        return ((Arithmetic) this.body).getRight();
+    }
+
+    public Function getLeftVariable() {
+        Function subFunction = ((Arithmetic) this.body).getLeft();
+        while (!subFunction.isVariable()) {
+            subFunction = ((Arithmetic) this.body).getLeft();
+        }
+        return subFunction;
+    }
+
+    public Function getLeftFunction() {
+        return ((Arithmetic) this.body).getLeft();
     }
 }
--- src/main/java/org/FunctionSimplifier/FunctionSimplifier.java
+++ src/main/java/org/FunctionSimplifier/FunctionSimplifier.java
@@ -6,6 +6,5 @@ public class FunctionSimplifier {
     public static void main(String[] args) {
         System.out.println("org.FunctionSimplifier.Function Simplifier");
         Addition test = new Addition(2, 3);
-        test.print();
     }
 }
