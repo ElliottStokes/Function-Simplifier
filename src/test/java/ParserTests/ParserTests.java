dd8741873532509536470f6b15f31f56515ed318
Elliott Stokes <erstokes1@sheffield.ac.uk>
2022-08-17 10:36:45 +0100

    Updated RPNParser to split version rather than toCharArray

--- src/test/java/ParserTests/ParserTests.java
+++ src/test/java/ParserTests/ParserTests.java
@@ -8,25 +8,33 @@ public class ParserTests {
     RPNParser rpnParser = new RPNParser();
     @Test
     public void operatorTests() {
-        Assertions.assertEquals("xy^", rpnParser.parse("x^y"));
-        Assertions.assertEquals("xy/", rpnParser.parse("x/y"));
-        Assertions.assertEquals("xy*", rpnParser.parse("x*y"));
-        Assertions.assertEquals("xy+", rpnParser.parse("x+y"));
-        Assertions.assertEquals("xy-", rpnParser.parse("x-y"));
+        Assertions.assertEquals("x y ^", rpnParser.parse("x ^ y"));
+        Assertions.assertEquals("x y /", rpnParser.parse("x / y"));
+        Assertions.assertEquals("x y *", rpnParser.parse("x * y"));
+        Assertions.assertEquals("x y +", rpnParser.parse("x + y"));
+        Assertions.assertEquals("x y -", rpnParser.parse("x - y"));
     }
 
     @Test
     public void basicFunctionTests() {
-        Assertions.assertEquals("xy+zp*-", rpnParser.parse("x + y - z * p"));
-        Assertions.assertEquals("xy*zx/+", rpnParser.parse("x * y + z / x"));
-        Assertions.assertEquals("xy^", rpnParser.parse("x ^ y"));
+        Assertions.assertEquals("x y + z p * -", rpnParser.parse("x + y - z * p"));
+        Assertions.assertEquals("x y * z x / +", rpnParser.parse("x * y + z / x"));
+        Assertions.assertEquals("x y ^", rpnParser.parse("x ^ y"));
     }
 
     @Test
     public void bracketTests() {
-        Assertions.assertEquals("xyzm-/+", rpnParser.parse("x + y / (z - m)"));
-        Assertions.assertEquals("xy-zm-/", rpnParser.parse("(x - y) / (z - m)"));
-        Assertions.assertEquals("xy+vx/y*-z-", rpnParser.parse("x + y - v / x * y - z"));
-        Assertions.assertEquals("xy+v-x/y*z-", rpnParser.parse("(((((x + y) - v) / x) * y) - z)"));
+        Assertions.assertEquals("x y ( z m - ) / +", rpnParser.parse("x + y / ( z - m )"));
+        Assertions.assertEquals("( x y - ) ( z m - ) /", rpnParser.parse("( x - y ) / ( z - m )"));
+        Assertions.assertEquals("x y + v x / y * - z -", rpnParser.parse("x + y - v / x * y - z"));
+        Assertions.assertEquals("( ( ( ( ( x y + ) v - ) x / ) y * ) z - )", rpnParser.parse("( ( ( ( ( x + y ) - v ) / x ) * y ) - z )"));
+    }
+
+
+    @Test
+    public void numbersTest() {
+        Assertions.assertEquals("5 10 +", rpnParser.parse("5 + 10"));
+        Assertions.assertEquals("x 5 ^", rpnParser.parse("x ^ 5"));
+        Assertions.assertEquals("5x 7x +", rpnParser.parse("5x + 7x"));
     }
 }
