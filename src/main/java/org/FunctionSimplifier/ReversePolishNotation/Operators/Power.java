89771701297e6c86eba48375c8fdc269352e99a1
Elliott Stokes <erstokes1@sheffield.ac.uk>
2022-08-25 11:36:59 +0100

    Updated power operator to allow simplifying

--- src/main/java/org/FunctionSimplifier/ReversePolishNotation/Operators/Power.java
+++ src/main/java/org/FunctionSimplifier/ReversePolishNotation/Operators/Power.java
@@ -3,6 +3,7 @@ package org.FunctionSimplifier.ReversePolishNotation.Operators;
 import org.FunctionSimplifier.SyntaxTree.BranchNode;
 import org.FunctionSimplifier.SyntaxTree.LeafNode;
 import org.FunctionSimplifier.SyntaxTree.Node;
+import org.FunctionSimplifier.Variable;
 
 public class Power implements Operator {
     private final int PRIORITY = 1;
@@ -16,27 +17,37 @@ public class Power implements Operator {
 
     @Override
     public Node evaluate(LeafNode leftNode, LeafNode rightNode) {
-        return null;
+        Variable leftNodeVariable = leftNode.getVariable();
+        return new LeafNode(new Variable(leftNodeVariable.getLabel(),
+                leftNodeVariable.getConstant(),
+                leftNodeVariable.getExponent() * rightNode.getVariable().getConstant()));
     }
 
     @Override
     public Node evaluate(BranchNode leftNode, BranchNode rightNode) {
-        return null;
+        return new BranchNode(this, leftNode, rightNode);
     }
 
     @Override
     public Node evaluate(LeafNode leftNode, BranchNode rightNode) {
-        return null;
+        return new BranchNode(this, leftNode, rightNode);
     }
 
     @Override
     public Node evaluate(BranchNode leftNode, LeafNode rightNode) {
-        return null;
+        return new BranchNode(this, leftNode, rightNode);
     }
 
     @Override
     public Node evaluate(Node leftNode, Node rightNode) {
-        return null;
+        if (leftNode instanceof LeafNode && rightNode instanceof LeafNode)
+            return evaluate((LeafNode) leftNode, (LeafNode) rightNode);
+        else if (leftNode instanceof LeafNode)
+            return evaluate((LeafNode) leftNode, (BranchNode) rightNode);
+        else if (rightNode instanceof LeafNode)
+            return evaluate((BranchNode) leftNode, (LeafNode) rightNode);
+        else
+            return evaluate((BranchNode) leftNode, (BranchNode) rightNode);
     }
 
     public String toString() {
